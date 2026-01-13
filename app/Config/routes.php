10a7e7807e764d1c89618b48a16ef099a8e9f3f5
erdibagus <erdibgs@gmail.com>
2026-01-13 12:22:34 +0700

    up

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -25,8 +25,10 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
-	Router::connect('/index', array('controller' => 'pages', 'action' => 'display', 'home'));
+	// Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	// Router::connect('/index', array('controller' => 'pages', 'action' => 'display', 'home'));
+	Router::connect('/', array('controller' => 'homes', 'action' => 'index'));
+	Router::connect('/index', array('controller' => 'homes', 'action' => 'index'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
